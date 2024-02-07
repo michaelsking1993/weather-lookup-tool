4f23259e337aa971e8449b5d3028f5294e135748
Michael King <michaelsking1993@gmail.com>
2024-02-07 10:49:49 -0500

    First commit: application basics, rspec installed

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,15 @@
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.clean_with :truncation, except: %w(ar_internal_metadata)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
